90caf9ac99bf75ebf6bdd02a222783875168942b
Aline França <aline.edvania.franca@outlook.com>
2024-08-05 07:26:41 -0300

    Criação do front das telas

--- src/main/java/com/sistema/gestao/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sistema.gestao.repository;
-
-import com.sistema.gestao.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByNome(String nome);
-}
